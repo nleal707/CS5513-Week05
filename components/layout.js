a9da6f566e5d25c85c7abd428eab678ee6c0d743
Nick Leal <business.nleal@gmail.com>
2025-09-13 10:24:05 -0700

    simple blog architecture

--- components/layout.js
+++ components/layout.js
@@ -59,11 +59,6 @@ export default function Layout({ children, home }) {
         )}
       </header>
       <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
-        </div>
-      )}
     </div>
   );
 }
